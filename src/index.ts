69073ebb8ee16305c5170f91aae5e139b32da7f5
Jose Daniel De La Cruz Rivera <danieldlcr5@gmail.com>
2022-06-10 08:07:20 -0500

    cambio de respositorio

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import userRouter from './routes/users'
+import cors from 'cors'
 const app = express()
 app.use(express.json())
 const PORT = 3000
+app.use(cors());
 
 app.use('/apiv1/users', userRouter)
 
